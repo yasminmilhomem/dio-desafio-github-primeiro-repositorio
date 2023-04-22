a7d822003df8d1c16b4bf065ec6a051888df3d5d
yasminmilhomem <yasminmilhomem1517@gmail.com>
2023-04-22 18:12:38 -0300

    segundo exercicio da Lista de Exercicios

---
+++ primeirosPassosComJavascript/listaDeExercicios/exercicios_02.js
@@ -0,0 +1,16 @@
+const peso = 55;
+const altura = 1.65;
+const imc = peso / Math.pow(altura, 2);
+console.log("Seu IMC é: " + imc.toFixed(2));
+
+if (imc < 18.5){
+    console.log("Abaixo do peso :c");
+} else if (imc >= 18.5 && imc < 25){
+    console.log("Peso normal c: ");
+} else if (imc >= 25 && imc < 30) {
+    console.log("Acima do peso :c");
+} else if (imc >= 30 && imc < 40) {
+    console.log("O B E S O !!");
+} else {
+    console.log("Obesidade grave :'(");
+}
